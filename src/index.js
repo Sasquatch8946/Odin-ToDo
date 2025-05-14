31ac59635d2e370e69aa4bc7e9140331369d1471
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-05-14 18:05:30 -0500

    Create displayController factory function with a few methods to create project sections and todo cards.

--- src/index.js
+++ src/index.js
@@ -17,11 +17,71 @@ class ToDo {
 	}
 }
 
+const displayController = (function () {
+
+	const getContainer = () => {
+		return document.querySelector(".todos");
+	};
+
+	const createProject = (projectName) => {
+		const container = getContainer();
+		const project = document.createElement("div");
+		project.innerText = projectName;
+		project.classList.add("project-section");
+		project.dataset.project = projectName;
+		container.appendChild(project);
+	}
+
+	const createTodoCard = (projectName, todo) => {
+		const container = document.querySelector(`.project-section[data-project=${projectName}`);
+		const cardTitle = document.createElement("div");
+		cardTitle.innerText = todo.title;
+		const cardDescription = document.createElement("div");
+		cardDescription.innerText = todo.description;
+		const cardDate = document.createElement("div");
+		cardDate.innerText = todo.dueDate;
+		const cardPriority = document.createElement("div");
+		cardPriority.innerText = todo.priority;
+		const cardChecklist = document.createElement("div");
+		cardChecklist.innerText = todo.checklist;
+		const cardNotes = document.createElement("div");
+		cardNotes.innerText = todo.notes;
+		container.appendChild(cardTitle);
+		container.appendChild(cardDescription);
+		container.appendChild(cardPriority);
+		container.appendChild(cardChecklist);
+		container.appendChild(cardNotes);
+	}
+
+	return {
+		createProject,
+		createTodoCard,
+	}
+})();
+
+let projects = {
+	today: []
+}
+
 let myToDo = new ToDo("Clean the floors",
 	"Sweep and wetjet the floors in the kitche, living room, and entryway.",
 	"5/15/25",
 	"high",
 	"move the furniture, sweep, wetjet",
 	"be thorough");
+
+console.log(myToDo);
+
+console.log(typeof projects.today);
+
+projects.today.push(myToDo);
+
+for (let project in projects) {
+	console.log(projects[project]);
+	displayController.createProject(project);
+	displayController.createTodoCard(project, projects[project][0]);
+}
+
+
 
 
